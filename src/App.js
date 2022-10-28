f71daf8913395ea5a1c51b8e22e716758da6db3a
Przemysław Prokopczuk <przemek.prokopczuk1@gmail.com>
2022-10-28 15:23:41 +0200

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './app.scss'
+import Sidebar from './components/sidebar/Sidebar';
+import Navbar from './components/navbar/Navbar';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <aside className="app-sidebar">
+        <Sidebar />
+      </aside>
+      <div className="app-wrapper">
+        <Navbar />
+      </div>
     </div>
   );
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,48 @@
 import './sidebar.scss'
+import {RocketLaunch, Dashboard, Group, ShoppingCart, Store, Person, Logout} from '@mui/icons-material';
+
 
 export default function Sidebar() {
     return (
-        <p>Sidebar</p>
+        <div className="sidebar">
+            <header className="logo">
+                <RocketLaunch className='logo__icon' sx={{fontSize: '1.8rem'}} />
+                <h1 className="logo__text"><span className="logo__text--highlight">Space</span>Admin</h1>
+            </header>
+            <hr className='sidebar__divider'/>
+            <body>
+                <nav className="nav">
+                    <ul className="nav__list">
+                        <p className="nav__title">MAIN</p>
+                        <li className="nav__item">
+                            <Dashboard />
+                            <span>Dashborad</span>
+                        </li>
+                        <p className="nav__title">LISTS</p>
+                        <li className="nav__item">
+                            <Group />
+                            <span>Users</span>
+                        </li>
+                        <li className="nav__item">
+                            <Store />
+                            <span>Products</span>
+                        </li>
+                        <li className="nav__item">
+                            <ShoppingCart />
+                            <span>Orders</span>
+                        </li>
+                        <p className="nav__title">USER</p>
+                        <li className="nav__item">
+                            <Person />
+                            <span>Profile</span>
+                        </li>
+                        <li className="nav__item">
+                            <Logout />
+                            <span>Logout</span>
+                        </li>
+                    </ul>
+                </nav>
+            </body>
+        </div>
     )
 }
