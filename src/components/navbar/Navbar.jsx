dbbd158c426d727d1a20046b0585e81a2bfd9a2b
Przemysław Prokopczuk <przemek.prokopczuk1@gmail.com>
2022-10-28 19:31:09 +0200

    add navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,23 @@
+import { Settings, NotificationsNone } from '@mui/icons-material'
 import './navbar.scss'
 
 export default function Navbar() {
     return (
-        <p>Navbar</p>
+        <header className="navbar-header">
+            <nav className="navbar">
+                <ul className="navbar__links">
+                    <li className="navbar__item">
+                        <NotificationsNone fontSize='medium' className='navbar__icon' />
+                        <span className='navbar__icon-badge'>9</span>
+                    </li>
+                    <li className="navbar__item">
+                        <Settings fontSize='medium' className='navbar__icon' />
+                    </li>
+                    <li className="navbar__item">
+                        <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3560&q=80' alt='avatar' className='navbar__avatar' />
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
