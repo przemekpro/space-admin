f76bc023f4ce2e174efe735c235620eb435479dd
Przemysław Prokopczuk <przemek.prokopczuk1@gmail.com>
2022-11-02 22:50:19 +0100

    add login page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import Sidebar from './components/sidebar/Sidebar';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home'
 import Users from './components/users/Users'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from './pages/login/login';
 
 
 function App() {
+
+  const loggedIn = true
+
+  function RequireAuth(props) {
+    return loggedIn ? props.children : <Navigate to='/login' />
+  }
+
   return (
     <div className="app">
       <aside className="app-sidebar">
@@ -15,10 +23,11 @@ function App() {
       <div className="app-wrapper">
         <Navbar />
         <div className="app-content">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/users' element={<Users />} />
-        </Routes>
+          <Routes>
+            <Route path='/' element={<RequireAuth><Home /></RequireAuth>} />
+            <Route path='/users' element={<RequireAuth><Users /></RequireAuth>} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
         </div>
       </div>
     </div>
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,51 @@
+import { useState } from 'react'
 import './login.scss'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Login() {
+
+    const [error, setError] = useState(false)
+    const [loginData, setLoginData] = useState({})
+
+    const navigate = useNavigate()
+
+    function onChange(e) {
+        setLoginData(prevValue => {
+            return {
+                ...prevValue, 
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    function handleLogin(e) {
+        e.preventDefault()
+        signInWithEmailAndPassword(auth, loginData.email, loginData.password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user)
+            navigate('/')
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setError(true)
+            console.log(errorCode)
+            console.log(errorMessage)
+        });
+    }
+
     return (
-        <h1>Login Page</h1>
+        <div className="login">
+            <form className="login__form" onSubmit={handleLogin}>
+                <input name='email' placeholder='email' type="email" className="login__input" onChange={onChange}/>
+                <input name='password' placeholder='password' type="password" className="login__input" onChange={onChange}/>
+                <button className="login__button">Login</button>
+            </form>
+            {error && <p className="login__error-msg">Wrong email or password</p>}
+        </div>
     )
 }
